97db75625c4855bd8e39242eac5b837dfed4b553
EriksJunior <EriksJunior13@gmail.com>
2021-09-28 19:00:46 -0300

    commit

---
+++ db/sql.js
@@ -0,0 +1,16 @@
+// CREATE TABLE `cliente` (
+//     `id` varchar(36) NOT NULL,
+//     `nome` varchar(40) NOT NULL,
+//     `nascimento` date DEFAULT NULL,
+//     `endereco` varchar(100) DEFAULT NULL,
+//     `numero` varchar(10) DEFAULT NULL,
+//     `cidade` varchar(30) DEFAULT NULL,
+//     `bairro` varchar(100) DEFAULT NULL,
+//     `uf` varchar(2) DEFAULT NULL,
+//     `cpfcnpj` varchar(18) DEFAULT NULL,
+//     `ie` varchar(20) DEFAULT NULL,
+//     `telefone` varchar(20) DEFAULT NULL,
+//     `celular` varchar(20) DEFAULT NULL,
+//     `obs` varchar(200) DEFAULT NULL,
+//     PRIMARY KEY (`id`)
+//   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
