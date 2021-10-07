c2b4d276f20180b8fdd6cde079a01e051d8037c0
EriksJunior <EriksJunior13@gmail.com>
2021-10-06 23:05:46 -0300

    commit

--- routes/produto.js
+++ routes/produto.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
-
+const uuid = require('uuid');
+const knex = require('../db/database')
 
 router.get('/', (req, res)=>{
 
     try {
-        res.status(200).render('home')
+        res.status(200).render('produto')
     } catch {
         res.status(500).send('ocorreu um erro');
     }
@@ -13,7 +14,4 @@ router.get('/', (req, res)=>{
 })
 
 
-
-
-
-module.exports = router;
+module.exports = router
--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const home = require('../routes/home')
 const cliente = require('../routes/cliente')
+const produto = require('../routes/produto')
 
 
 
 router.use('/', home)
 router.use('/cliente', cliente)
+router.use('/produto', produto)
 
 module.exports = router;
