6642504370553489f229d4797085c0f2efedd5d2
EriksJunior <EriksJunior13@gmail.com>
2021-10-11 21:03:46 -0300

    commit

--- public/js/produto.js
+++ public/js/produto.js
@@ -1,9 +1,5 @@
-document.querySelector('#salvarCliente').addEventListener('click',  (e) => {
+document.querySelector('#salvarCliente').addEventListener('click', async (e) => {
     e.preventDefault()
-    salvarProduto()
-})
-
-async function salvarProduto(){
     try {
         const nome = document.querySelector('#nome').value;
         const valorUnitario = document.querySelector('#valorUnitario').value;
@@ -22,6 +18,53 @@ async function salvarProduto(){
 
         const { data } = await axios.post('/produto/salvarProduto', dadosProduto)
         console.log(data)
+        alert('Produto Cadastrado')
+        document.querySelector('#nome').value = '';
+        document.querySelector('#valorUnitario').value = '';
+        document.querySelector('#unidade').value = '';
+        document.querySelector('#estoque').value = '';
+        document.querySelector('#marca').value ='';
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+document.querySelector('#btnRealizarBusca').addEventListener('click', async (e)=>{
+    try {
+        e.preventDefault();
+        const {data} = await axios.get('/produto/buscarProduto')
+        console.log(data)
+    
+        $("#tabelaInfoProduto tbody").empty() // limpa a tabela pq se naõ acada hora que clicar na pesquisa insere duplicado
+
+        for (const item of data) {
+            $("#tabelaInfoProduto tbody").append(` 
+            <tr>
+                 <td style = "display: none">${item.id}</td>
+                 <td >${item.nome}</td>
+                 <td style = "text-align: center">${parseInt(item.valorUnitario || '0,00').toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
+                 <td style = "text-align: center">${item.unidade}</td>
+                 <td style = "text-align: center">${item.estoque  || '0'}</td>
+                 <td style = "text-align: center">${item.marca}</td>
+                 <td><button id="btnEditarProduto" onclick= "editarProduto('${item.id}')">Editar</button></td>
+                 <td><button id="btnExcluirProduto" onclick= "excluirProduto('${item.id}')">Excluir</button></td>
+            </tr>
+            `)
+        }
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+async function editarProduto(id){
+    try {
+        const {data} = await axios.get(`/produto/editarProduto/${id}`)
+        console.log(data)
+
+
+        return data
     } catch (error) {
         console.log(error)
     }
--- routes/produto.js
+++ routes/produto.js
@@ -19,9 +19,9 @@ router.post('/salvarProduto', async (req, res) => {
         const id = uuid.v4();
         const dadosProduto = { id, nome, valorUnitario, unidade, estoque, marca }
         await knex
-        .table('produto')
-        .insert(dadosProduto)
-        
+            .table('produto')
+            .insert(dadosProduto)
+
 
         return res.status(200).json(dadosProduto)
 
@@ -31,5 +31,29 @@ router.post('/salvarProduto', async (req, res) => {
     }
 })
 
+router.get('/buscarProduto', async (req, res) => {
+    try {
+        const data = await knex.table('produto')
+            .select('produto.id', 'produto.nome', 'produto.valorUnitario', 'produto.unidade', 'produto.estoque', 'produto.marca')
+        console.log(data)
+        return res.status(200).json(data)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
 
+router.get('/editarProduto/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const data = await knex
+            .table('produto')
+            .select('produto.id', 'produto.nome', 'produto.valorUnitario', 'produto.unidade', 'produto.estoque', 'produto.marca')
+            .where('id', '=', id)
+            return res.status(200).json(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 module.exports = router
